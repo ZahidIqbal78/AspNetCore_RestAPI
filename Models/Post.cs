f23fb9b372d73413b389e91523e7b64c547f8b98
ZahidIqbal78 <zi.tushar@live.com>
2022-05-07 05:58:42 -0700

    changed from string to guid for id property, added name property

--- Models/Post.cs
+++ Models/Post.cs
@@ -2,6 +2,7 @@ namespace AspNetCore_RestAPI.Models
 {
     public class Post
     {
-        public string Id { get; set; }
+        public Guid Id { get; set; }
+        public string Name { get; set; }
     }
 }
