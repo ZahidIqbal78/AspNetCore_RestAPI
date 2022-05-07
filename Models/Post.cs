c1eade5e1da4622fb532014114798cd1c50ced5b
ZahidIqbal78 <zi.tushar@live.com>
2022-05-07 23:10:55 +0600

    added note

--- Models/Post.cs
+++ Models/Post.cs
@@ -1,5 +1,8 @@
 namespace AspNetCore_RestAPI.Models
 {
+    /// <summary>
+    /// This model is for demonstration only, not a domain or database model.
+    /// </summary>
     public class Post
     {
         public Guid Id { get; set; }
